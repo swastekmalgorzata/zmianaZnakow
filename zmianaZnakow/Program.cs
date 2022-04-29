8cfe26c40974a7597a7949222ae4993d1d9bd81d
swastek_malgorzata <swastekm@gmail.com>
2022-04-29 19:14:18 +0200

    Add project files.

---
+++ zmianaZnakow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zmianaZnakow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("podaj slowo; ");
+            string slowo =Console.ReadLine();
+            char[] tab = new char[slowo.Length];
+            for(int i =0; i<slowo.Length;i++)
+            {
+                tab[i] = slowo[i]; 
+            }
+            char s;
+            s = tab[0];
+            tab[0] = tab[tab.Length - 1];
+            tab[tab.Length - 1] = s;
+            foreach(char x in tab)
+            {
+                Console.Write(x);
+            }
+
+           Console.ReadKey();
+            
+
+
+        }
+    }
+}
